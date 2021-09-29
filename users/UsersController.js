8f7c9df2e19c02c17bd5602740d1fce614fd1851
Anderson Jorge <anderson.jorge33@gmail.com>
2021-09-28 21:23:17 -0300

    atualização do projeto: criação de hash de senha

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./user');
+const bcrypt = require('bcryptjs');
 
 router.get('/admin/users', (req, res) => {
   res.send('<h1>Welcome!</h1>');
@@ -8,6 +9,23 @@ router.get('/admin/users', (req, res) => {
 
 router.get('/admin/users/create', (req, res) => {
   res.render('admin/users/create');
-})
+});
+
+router.post('/users/create', (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+
+  let salt = bcrypt.genSaltSync(10);
+  let hash = bcrypt.hashSync(password, salt);
+
+  User.create({
+    email: email,
+    password: hash
+  }).then(() => {
+    res.redirect('/');
+  }).catch(err => {
+    res.redirect('/');
+  });
+});
 
 module.exports = router;
