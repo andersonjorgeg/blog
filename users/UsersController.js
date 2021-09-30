84d3e0b308d7aea9668685c83cbf6184bd95110d
Anderson Jorge <anderson.jorge33@gmail.com>
2021-09-30 17:14:02 -0300

    atualização do projeto: edição do usuário

--- users/UsersController.js
+++ users/UsersController.js
@@ -4,7 +4,11 @@ const User = require('./user');
 const bcrypt = require('bcryptjs');
 
 router.get('/admin/users', (req, res) => {
-  res.send('<h1>Welcome!</h1>');
+  User.findAll().then(users => {
+    res.render('admin/users/index', {
+      users: users
+    });
+  })
 });
 
 router.get('/admin/users/create', (req, res) => {
@@ -15,17 +19,58 @@ router.post('/users/create', (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
 
-  let salt = bcrypt.genSaltSync(10);
-  let hash = bcrypt.hashSync(password, salt);
+  User.findOne({
+    where: {email: email}
+  }).then(user => {
+    if(user == undefined) {
+      let salt = bcrypt.genSaltSync(10);
+      let hash = bcrypt.hashSync(password, salt);
 
-  User.create({
-    email: email,
-    password: hash
-  }).then(() => {
-    res.redirect('/');
-  }).catch(err => {
-    res.redirect('/');
+      User.create({
+        email: email,
+        password: hash
+      }).then(() => {
+        res.redirect('/');
+      }).catch(err => {
+        res.redirect('/');
+      });
+    } else {
+      res.redirect('/admin/users/create');
+      
+    }
   });
 });
+
+router.get('/admin/users/edit/:id', (req, res) => {
+  let id = req.params.id;
+
+  if(isNaN(id)){
+    res.redirect('/admin/users');
+  }
+
+  User.findByPk(id).then(user => {
+    if(user != undefined){
+      res.render('admin/users/edit', {
+        user: user
+      })
+    } else {
+      res.redirect('admin/users');
+    }
+  }).catch(err => {
+    res.redirect('admin/users');
+  });
+});
+
+router.post('/users/update', (req, res) => {
+  let id = req.body.id;
+  let email = req.body.email;
+  let password = req.body.password;
+
+  User.update({email: email, password: password}, {
+    where: {id: id}
+  }).then(() => {
+    res.redirect('/admin/users');
+  })
+})
 
 module.exports = router;
